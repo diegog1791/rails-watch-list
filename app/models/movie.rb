04d5d6cde229b20d92d781b3be300f235cd93c7b
diego garza <diego.garza17@gmail.com>
2021-05-20 16:37:42 -0500

    Creating watch list

---
+++ app/models/movie.rb
@@ -0,0 +1,9 @@
+class Movie < ApplicationRecord
+  
+  validates :title, uniqueness: true, presence: true
+  validates :overview, presence: true
+
+  has_many :bookmarks
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get "lists", to: "lists#index"
+  # get "lists/:id", to: "lists#show"
+  # get "lists/new", to: "lists#new"
+  # post "lists", to: "lists#create"
+
+  # get "lists/:id/bookmarks/new", to: "lists#edit"
+  # patch "lists/:id/bookmarks", to: "lists#update"
+  # delete "bookmarks/:id", to: "lists#destroy"
+  # root to: "list#index"
+  resources :movies
+
+  resources :lists do 
+    resources :bookmarks, only: [:create, :new]
+  end
+  resources :bookmarks, only: [:destroy]
 end
